384148480c724d0468e66157c17d1c5f204c538c
PedroWin <pricci1@miuandes.cl>
2017-12-05 00:40:30 -0300

    varios bindings en CrearPisos

--- POOmall/View/CrearPisosViewModel.cs
+++ POOmall/View/CrearPisosViewModel.cs
@@ -10,12 +10,68 @@ namespace POOmall.View
 {
     public class CrearPisosViewModel
     {
+        public CrearPisosViewModel()
+        {
+            AddTiendaCommand = new RelayCommand(OnAddTienda, CanAddTienda);
+            AddPisoCommand = new RelayCommand(OnAddPiso, CanAddPiso);
+        }
+
+        
         public ObservableCollection<Piso> Pisos
         {
             get => Settings.Pisos;
             set => Settings.Pisos = value;
         }
 
-        public Piso SelectedPiso;
+        public ObservableCollection<Tienda> Tiendas
+        {
+            get => Settings.Tiendas;
+            set => Settings.Tiendas = value;
+        }
+
+        //public Piso SelectedPiso;
+        private Piso _selectedPiso;
+        public Piso SelectedPiso
+        {
+            get => _selectedPiso;
+            set
+            {
+                _selectedPiso = value;
+                AddTiendaCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string NumeroPisoSeleccionado => $"Piso: {SelectedPiso.Numero}";
+
+        #region AddPiso
+        public RelayCommand AddPisoCommand { get; }
+
+        private bool CanAddPiso()
+        {
+            return true;
+        }
+
+        private void OnAddPiso()
+        {
+            Pisos.Add(new Piso(1,1000));
+        }
+
+        #endregion
+
+
+        #region AddTienda
+        public RelayCommand AddTiendaCommand { get; private set; }
+
+        private bool CanAddTienda()
+        {
+            return SelectedPiso != null;
+        }
+
+        private void OnAddTienda()
+        {
+            Tiendas.Add(new Tienda("Test", 12,12,100,1,10,Settings.Categoria.Ferreteria));
+        }
+        #endregion
+
     }
 }
