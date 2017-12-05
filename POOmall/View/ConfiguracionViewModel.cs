d33ef0a33a7712c70825713059117d3c2c5a6791
PedroWin <pricci1@miuandes.cl>
2017-12-04 23:16:29 -0300

    ConfiguracionView

---
+++ POOmall/View/ConfiguracionViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOmall.View
+{
+    public class ConfiguracionViewModel
+    {
+        public int PrecioArriendo
+        {
+            get => Settings.PrecioArriendo;
+            set => Settings.PrecioArriendo = value;
+        }
+
+        public int DineroInicial
+        {
+            get => Settings.DineroInicial;
+            set => Settings.DineroInicial = value;
+        }
+
+        public int HorasFuncionamiento
+        {
+            get => Settings.HorasFuncionamiento;
+            set => Settings.HorasFuncionamiento = value;
+        }
+
+        public int SueldoEmpreados
+        {
+            get => Settings.SueldoEmpreados;
+            set => Settings.SueldoEmpreados = value;
+        }
+
+        public ConfiguracionViewModel()
+        {
+            PrecioArriendo = 100;
+            DineroInicial = 100;
+            HorasFuncionamiento = 12;
+            SueldoEmpreados = 10;
+        }
+    }
+}
--- POOmall/View/CrearPisosViewModel.cs
+++ POOmall/View/CrearPisosViewModel.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using POOmall.Model;
 
 namespace POOmall.View
 {
     public class CrearPisosViewModel
     {
+        public ObservableCollection<Piso> Pisos
+        {
+            get => Settings.Pisos;
+            set => Settings.Pisos = value;
+        }
+
     }
 }
